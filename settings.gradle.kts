f724bf9494e30b24b92d2ba771c496528be5bf1d
BryanRoss1 <bryanpross76@gmail.com>
2023-10-14 08:54:34 -0600

    first commit, added db.json

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ross Week6 REST API"
+include(":app")
